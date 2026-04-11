e4ebb53b8a036a83708199e3097c0d0c4cc7e892
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-04-11 14:54:18 +0530

    by vikram

--- client/src/pages/RedeemPage.jsx
+++ client/src/pages/RedeemPage.jsx
@@ -37,7 +37,7 @@ const RedeemPage = () => {
   {/* 👇 MESSAGE BELOW */}
   {success && (
   <span className="text-xs text-green-400 mt-1">
-    {success.message}
+    Code redeemed successfully
   </span>
 )}
 
