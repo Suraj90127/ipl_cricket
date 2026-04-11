71fa041c2795ca49d07dafe86cf8fecd0572dcd7
Vikram Thakur <trivikramkumar@maxifysolution.com>
2026-04-11 13:25:24 +0530

    error solved

--- client/src/pages/RedeemPage.jsx
+++ client/src/pages/RedeemPage.jsx
@@ -36,16 +36,16 @@ const RedeemPage = () => {
 
   {/* 👇 MESSAGE BELOW */}
   {success && (
-    <span className="text-xs text-green-400 mt-1">
-      Claimed successfully ✅
-    </span>
-  )}
+  <span className="text-xs text-green-400 mt-1">
+    {success.message}
+  </span>
+)}
 
-  {error && (
-    <span className="text-xs text-red-400 mt-1">
-      First need to recharge
-    </span>
-  )}
+{error && (
+  <span className="text-xs text-red-400 mt-1">
+    {error}
+  </span>
+)}
 
 </div>
   );
